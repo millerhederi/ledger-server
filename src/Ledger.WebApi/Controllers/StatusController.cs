98d89f5e2f74859fc3b183a8c2d2a1958257e76b
Miller Hederi <millerhederi@gmail.com>
2020-04-04 17:23:28 -0400

    Create stubbed Ledger solution with Ledger.WebApi project

---
+++ src/Ledger.WebApi/Controllers/StatusController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ledger.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        [Route("ping")]
+        public string Get()
+        {
+            return "pong";
+        }
+    }
+}
